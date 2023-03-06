5343921c09696e7f5476a2c2afe36808535601d7
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-03-06 14:04:40 +0000

    20230306.23:04

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'post_images/new'
+  get 'post_images/index'
+  get 'post_images/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
-get 'homes/about' => 'homes#about'
-get 'about' => 'homes#about'
+
+resources :post_images,only:[:new,:create,:index,:show]
+
+get 'homes/about' => 'homes#about' , as:"about"
+
 end
